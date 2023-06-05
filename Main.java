69ec9c8ab079cc87bbcbcc14c20a5a8408e9395a
reilolmsted <ROlmsted8132@student.whatcom.edu>
2023-06-04 19:33:04 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+// Reilly Olmsted
+//5-2-23
+//CS145
+//Lab 6 Binary Search Tree
+
+public class Main{// Beginning of test class
+
+    public static void main(String [] args){//main method
+        Interface  x = new Interface();
+        x.dictionaryMenu();//calls to user menu
+
+    }//end of main method
+
+} //end of Main
